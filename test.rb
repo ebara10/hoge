82e75c2319a430ddca80fdeb31e0cbce8cf49f88
Chihiro Ebara <ebara10@c.kyoai.ac.jp>
2017-04-22 12:38:02 +0900

    test.rb追加した

---
+++ test.rb
@@ -0,0 +1,9 @@
+begin
+#  File.open('readfile.txt') do |file|
+#    file.each_line do |line|
+      line = "Rive University"
+      strAry = line.split(" ")
+      puts strAry[0]
+#    end
+#  end
+end
